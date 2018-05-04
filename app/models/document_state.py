48ae323324101d584a928177ece2f23e975000a1
Olexii Lukianchuk <olexii.lykianchyk@gmail.com>
2018-05-04 10:13:05 +0300

    Added raw version of document model and it's state

---
+++ app/models/document_state.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, schema
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import Boolean, Date, Integer, String, LargeBinary
+
+
+Base = declarative_base()
+
+
+class DocumentState(Base):
+    __tablename__ = 'document_state'
+
+    id = Column('state_id', Integer, primary_key=True)
+    name = Column(String(15))
+    description = Column(String(255))
